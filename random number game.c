a1bf185b98ce74a3c85a98a6e5d1cbd55b254514
shaili-dixit <dixitshaili5@gmail.com>
2026-06-04 11:01:46 +0530

    Implement random number generation in game

--- random number game.c
+++ random number game.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main() {
+    // Seed the random number generator using the current system time
+    srand(time(0)); 
+    
+    // Generate a random number between 1 and 100
+    int secret_number = (rand() % 100) + 1; 
+    
+    printf("Random Number: %d", secret_number);
+    return 0;
+}
